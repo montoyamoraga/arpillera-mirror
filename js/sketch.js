f6f037246db0fae99094e9741066e438edfe4148
montoyamoraga <montoyamoraga@gmail.com>
2019-05-29 16:37:13 -0400

    add new arpillera

--- js/sketch.js
+++ js/sketch.js
@@ -51,8 +51,11 @@ let images = [];
 
 // p5.js function
 function setup() {
+
   // p5.js function to create canvas
   let canvas = createCanvas(320, 240).parent('canvasContainer');
+
+  // center canvas
   let x = (windowWidth - width) / 2;
   let y = (windowHeight - height) / 2;
   canvas.position(x, y);
@@ -67,9 +70,6 @@ function setup() {
   resultImg = createImg('');
   resultImg.hide();
 
-  // The button to start and stop the transfer process
-  // select('#startStop').mousePressed(startStop);
-
   // load models
   for (let i = 0; i< stylesNames.length; i++) {
     styles.push(ml5.styleTransfer(stylesNames[i], video, modelLoaded));
@@ -80,17 +80,6 @@ function setup() {
     images.push(document.getElementById("img" + i));
   }
 
-  // hide all pictures except for 0th:
-  // for (let i = 1; i < images.length; i++) {
-  //   images[i].hidden = true;
-  // }
-
-  // Create a new Style Transfer method with a defined style.
-  // We give the video as the second argument
- // style = ml5.styleTransfer('models/udnie', video, modelLoaded);
-
-  // style = ml5.styleTransfer('models/02-contra-la-guerra', video, modelLoaded);
-
   playgroundDiv = document.getElementById("playgroundDiv");
 
 }
@@ -98,11 +87,9 @@ function setup() {
 function draw(){
   // Switch between showing the raw camera or the style
   if (isTransferring) {
-    // image(resultImg, 0, 0, 320, 240);
     image(resultImg, 0, 0, 320, 240);
   } else {
-    // image(video, 0, 0, 320, 240);
-      image(video, 0, 0, 320, 240);
+    image(video, 0, 0, 320, 240);
   }
 }
 
@@ -126,14 +113,7 @@ function startStop() {
 }
 
 function startStyle(style) {
-  // if (isTransferring) {
-    // select('#startStop').html('Start');
-  // } else {
-    // select('#startStop').html('Stop');
-    // Make a transfer using the video
    styles[style].transfer(gotResult);
-  // }
-  // isTransferring = !isTransferring;
 }
 
 // When we get the results, update the result image src
@@ -148,25 +128,12 @@ function keyPressed() {
   // substract 48 to retrieve number
   let numberKey = keyCode - 48;
   // check that key is a number
-  if (numberKey >= 1 && numberKey <=8) {
+  if (numberKey >= 1 && numberKey <=9) {
     // update currentModel
     currentModel = numberKey - 1;
 
     let auxText = "background-image:url(images/" + imagesOriginalNames[currentModel] + ".jpg)"
 
     playgroundDiv.style = auxText;
-
-    //update image, hide all and only make one visible
-    // for (let i = 0; i < images.length; i++) {
-    //   images[i].hidden = true;
-    // }
-    // images[currentModel].hidden = false;
-
-
-
-  }
-  // TODO: retrieve spacebar to toggle style transfer
-  if (keyCode == " ") {
-    console.log("yo space");
   }
 }
